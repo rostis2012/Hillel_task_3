6c62a6a711373f3b30bd8de67dc9e0ed38c0b1b7
rostis2012 <rostis2012@gmail.com>
2022-10-20 19:11:07 +0300

    task_3_v.04 (переработал, убрал quit, оптимизировал создание переменных, оптимизировал создание строковых переменных (код длиннее, зато меньше памяти использует)

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ if input_string.replace(" ", "").isdigit(): # проверка на цифры
     values = input_string.split()
     count = len(values)
     if count <= 3:  # Проверка кол-ва сторон
-        perim_str = 'Периметр = '
-        squere_str = 'Площадь = '
-        side_a_str = 'Сторона a = '
-        side_b_str = 'Сторона b = '
-        side_c_str = 'Сторона c = '
         if count == 1:
             if int(input_string):  # проверка на 0
                 type_str = 'Круг:'
                 rad_str = 'Радиус = '
+                perim_str = 'Периметр = '
+                squere_str = 'Площадь = '
                 radius = int(input_string)
                 perimetr = 2 * 3.14 * radius
                 sq = 3.14 * (radius * radius)
@@ -23,13 +20,18 @@ if input_string.replace(" ", "").isdigit(): # проверка на цифры
             if int(a) and int(b):  # проверка на 0
                 side_a = int(a)
                 side_b = int(b)
+                perim_str = 'Периметр = '
+                squere_str = 'Площадь = '
                 if side_a == side_b: #проверка на квадрат
+                    side_a_str = 'Сторона a = '
                     type_str = 'Квадрат:'
                     perimetr = 4 * side_a
                     sq = side_a * side_a
                     print(f'{type_str} {side_a_str} {side_a}; {perim_str} {round(perimetr, 2)}, {squere_str} {round(sq, 2)}')
                 else: # иначе прямоугольник
                     type_str = 'Прямоугольник:'
+                    side_a_str = 'Сторона a = '
+                    side_b_str = 'Сторона b = '
                     perimetr = 2 * (side_a + side_b)
                     sq = side_a * side_b
                     print(
@@ -44,6 +46,11 @@ if input_string.replace(" ", "").isdigit(): # проверка на цифры
                     side_b = int(b)
                     side_c = int(c)
                     type_str = 'Треугольник:'
+                    perim_str = 'Периметр = '
+                    squere_str = 'Площадь = '
+                    side_a_str = 'Сторона a = '
+                    side_b_str = 'Сторона b = '
+                    side_c_str = 'Сторона c = '
                     perimetr = side_a + side_b + side_c
                     p_p = perimetr / 2
                     sq = (p_p * ((p_p - side_a) * (p_p - side_b) * (p_p - side_c))) ** 0.5
